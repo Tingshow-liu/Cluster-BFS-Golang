fe83d1cc9004aaf50458d087b0f9ee6bae42f527
Claire Liu <tl4151@nyu.edu>
2025-04-22 23:52:08 -0400

    Apply new utilties to read massive graph data

--- main.go
+++ main.go
@@ -1,70 +1,62 @@
 package main
 
 import (
-	"bufio"
+	"cluster_bfs_go/graphutils"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
-// Read input test data and convert to adjacent list
-func readAdjList(path string) [][]int {
-	file, err := os.Open(path)
-	if err != nil {
-		panic(err) // Immediately abort the program and prints the error message
-	}
-	defer file.Close() // Schedules to run automatically when readAdjList returns, no matter it returns normally or panics
-
-	var G [][]int                // Output graph (Adjacent list)
-	sc := bufio.NewScanner(file) // Create a scanner to read the file line by line
-	for sc.Scan() {              // Iterate over each non-empty line
-		tok := strings.Fields(sc.Text()) // Split the line on spaces
-		if len(tok) == 0 {               // Ignore empty lines in the text file
-			continue
-		}
-		u, _ := strconv.Atoi(tok[0]) // First token is the source vertex u
-		for len(G) <= u {            // Guarantee G is long enough so that index u exists
-			G = append(G, nil)
-		}
-		for _, s := range tok[1:] {
-			v, _ := strconv.Atoi(s)
-			G[u] = append(G[u], v)
-		}
-	}
-	// Final error check after scanning the file
-	if err := sc.Err(); err != nil {
-		panic(err)
-	}
-	return G
-}
-
-// Run the test
+// Read the bin files and print part of the graph
 func main() {
-	G := readAdjList("data/test.txt")
-	// Create the transpose of G
-	GT := make([][]int, len(G))
-	for u, neighbors := range G {
-		for _, v := range neighbors {
-			GT[v] = append(GT[v], u)
-		}
+	// Read input data and construct the graph
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s graph.bin\n", os.Args[0])
+		os.Exit(1)
+	}
+	path := os.Args[1]
+
+	// start := time.Now()
+	offsets, edges, err := graphutils.ReadGraphFromBin(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading graph: %v\n", err)
+		os.Exit(1)
+	}
+	// fmt.Printf("Load time: %v\n\n", time.Since(start))
+
+	// Print first 5 vertices (or fewer, if n<5)
+	n := len(offsets) - 1
+	limit := 5
+	if n < limit {
+		limit = n
+	}
+	for i := 0; i < limit; i++ {
+		startIdx := offsets[i]
+		endIdx := offsets[i+1]
+		neighbors := edges[startIdx:endIdx]
+		fmt.Printf("Vertex %3d has %2d neighbors: %v\n",
+			i, len(neighbors), neighbors)
 	}
 
-	cbfs := &ClusterBFS{G: G, GT: GT, R: 2} // cbfs: a ClusterBFS object stored by pointer
-	seeds := cbfs.Init([]int{0, 2})         // picks {0,2} as seeds
-	cbfs.RunBFS(seeds)
-
-	fmt.Println("Distances D:", cbfs.D)
-	fmt.Println("Seed masks S:", cbfs.S)
-	fmt.Println("G:", G)
-	fmt.Println("GT:", GT)
+	// Seeds selection
 }
 
-/* Functions to be parallelized */
-// AddVertices
-// ToSeq
-// CountTrue
+// // Run the test for our own tiny graph (data/test.txt) (TBD)
+// func main() {
+// 	G := utils.ReadAdjList("data/test.txt")
+// 	// Create the transpose of G
+// 	GT := make([][]int, len(G))
+// 	for u, neighbors := range G {
+// 		for _, v := range neighbors {
+// 			GT[v] = append(GT[v], u)
+// 		}
+// 	}
 
-// NewEdgeMap
-// EdgeMapSparse, EdgeMapDense
-// Run
+// 	cbfs := &ClusterBFS{G: G, GT: GT, R: 2} // cbfs: a ClusterBFS object stored by pointer
+// 	seeds := cbfs.Init([]int{0, 2})         // picks {0,2} as seeds
+// 	cbfs.RunBFS(seeds)
+
+// 	fmt.Println("Distances D:", cbfs.D)
+// 	fmt.Println("Seed masks S:", cbfs.S)
+// 	fmt.Println("G:", G)
+// 	fmt.Println("GT:", GT)
+// }
